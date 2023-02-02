8b234c4cd0e301ff198870ffa1d8fab1142c3d74
ckxingchen <1934433596@qq.com>
2023-02-02 08:48:37 +0800

    edit api_key

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # pyqt5-chatgpt
 基于chatGPT的pyqt5图形界面程序
+
+
+
+## 界面展示：
+
+![image-20230202084725426](images/image-20230202084725426.png)
+
+输入人工提问，点击提交。
+
+
+
+自动化测试、测试开发收徒：ck_xingchen
--- myAiAnswerWidget.py
+++ myAiAnswerWidget.py
@@ -108,7 +108,7 @@ class QmyAiAnswerWidget(QWidget):
         super().__init__(parent)           # 调用父类的构造函数，创建QWidget窗体
         self.ui = Ui_aiAnswerWidget()      # 创建UI对象
         self.ui.setupUi(self)              # 构造UI
-        self.api_keys = "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"
+        self.api_keys = "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"
         self.question = ""                 # 问题
         self.thread = None
         self.customConfigUI()
